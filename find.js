8276ad0e4a24e0d0a74be5e3a4d100ca88a68608
chao li <chao li>
2016-05-30 00:49:22 +0800

    first commit

---
+++ find.js
@@ -0,0 +1,59 @@
+//find all .ts file to be compiling
+var path = require("path");
+var fs = require('fs');
+
+
+function checkFileStat(pathname) {
+    try {
+        var stat = fs.statSync(pathname);
+        if (stat.isFile()) {
+            return 1;
+        }
+        else if (stat.isDirectory()) {
+            return 2;
+        }
+        else
+            return undefined;
+    }
+    catch (e) {
+        console.log(e);
+        return undefined;
+    }
+}
+
+var tslist = [];
+
+function addToList(pathname){
+    tslist.push(pathname);
+}
+function saveAll(){
+    var content = tslist.join("\r\n");
+    fs.writeFileSync("input.txt", content);
+}
+
+function travalDir(dirname, cb){
+    var i;
+    var filelist = fs.readdirSync(dirname);
+    for (i = 0; i < filelist.length; i++) {
+        var fullname = path.join(dirname, filelist[i]);
+        var filetype = checkFileStat(fullname);
+        if (filetype == 1) {
+            if (path.extname(fullname) === ".ts") {
+                cb(fullname);
+            }
+        }
+        else if (filetype == 2) {
+            travalDir(fullname, cb);
+        }
+    }
+}
+
+function travalAllTs(pathname)
+{
+    var cwd = pathname || process.cwd();
+    var curlist = fs.readdirSync(cwd);
+    travalDir(cwd, addToList);
+    saveAll();
+}
+
+travalAllTs();
